096bb629cfe581aa456ef8fce10acd501c803e1c
mohiljoshi24 <mohiljoshi245@outlook.com>
2026-07-14 23:26:10 +0530

    Update marks.c
    created a very simple program where we take the input from user about their academic marks and create their marksheet

--- marks.c
+++ marks.c
@@ -3,13 +3,21 @@ int main () {
     int math;
     int phy;
     int chem;
+    int comp;
+    int english;
     printf("enter math marks: \n");
     scanf("%d", &math);
     printf("enter phy marks:- \n");
      scanf("%d", &phy);
     printf("enter chem marks: \n");
      scanf("%d", &chem);
+      printf("enter comp marks: \n");
+     scanf("%d", &comp);
+      printf("enter english marks: \n");
+     scanf("%d", &english);
      printf("math:%d\n", math);
      printf("phy:%d\n", phy);
      printf("chem:%d\n", chem);
+      printf("comp:%d\n", comp);
+       printf("english:%d\n", english);
 }
