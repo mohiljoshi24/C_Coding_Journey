c8ed4c50a17c1f248dd69e664fe373233e961288
mohiljoshi24 <mohiljoshi245@outlook.com>
2026-07-16 17:41:20 +0530

    Update numberchecker.c
    a simple program to check if the given integer is positive, negative or zero

--- numberchecker.c
+++ numberchecker.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
 int main(){
+
     int num1;
+
     printf("enter your integer number to check if its positive or negative\n");
 scanf("%d", &num1);
+
  int positive = num1 > 0;
      int negative = num1 < 0;
     int equal = num1 == 0;
+
 if (positive) {
     printf(" this number %d is a positive integer\n", num1);
 }
+
 else if (negative){
     printf (" this number %d is a negative integer\n", num1);
 
 }
+
 else if (equal){ 
     printf(" this number %d is zero \n", num1);
 }
+
 }
